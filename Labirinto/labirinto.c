18ab298c595c4e8b9da0e32650da6d9518939e33
andreinamendes <andreinamendes63@gmail.com>
2019-03-02 22:41:24 -0300

    README add

--- Labirinto/labirinto.c
+++ Labirinto/labirinto.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+const char TREE = '#';
+const char FIRE = 'o';
+const char EMPTY = '.';
+
 int main(){
 
+    int nCol = 0;
+    int nLin = 0;
+    int Lin_i = 0;
+    int Col_i = 0;
+    scanf("%d %d %d %d", nCol, nLin, Lin_i, Col_i);
+
+    int matriz[nLin][nCol];
 
     return 0;
 }
